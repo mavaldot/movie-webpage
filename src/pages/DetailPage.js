a990d7e156ad32d11e77b480b6aad99fb952e7f8
mavaldot <48870467+mavaldot@users.noreply.github.com>
2022-09-26 16:11:57 -0500

    Add details page

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,22 +1,31 @@
 import { useParams, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { getMovie } from "../util/requests";
+import { Button, Navbar } from 'reactstrap'
+
 
 const DetailPage = () => {
 
-    const APPURL = process.env.REACT_APP_URL;
-    let sessionId = sessionStorage.getItem('sessionId') || '';
+    let { id } = useParams();
 
-    // const axios = require('axios').default;
-    // const API_KEY = process.env.REACT_APP_API_KEY
-
-    const [searchParams, setSearchParams] = useSearchParams();
-
-   let { id } = useParams();
-   console.log(id);
+    const imgLink = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/';
 
     const [isLoading, setLoading] = useState(true);
+    const [movie, setMovie] = useState({});
+    const [userRating, setUserRating] = useState();
 
     useEffect(() => {
+        const getInfo = async () => {
+            let res = await getMovie(id);
+            setMovie(res);
+        };
+
+        const getRating = async () => {
+
+        };
+
+        getInfo();
+
         setLoading(false);
     }, []);
 
@@ -27,7 +36,23 @@ const DetailPage = () => {
     else {
         return (
             <div>
-                <p>{id}</p>
+                <Navbar>
+                    <Button href='/' color='danger'>Go back</Button>
+                </Navbar>
+                <img src={imgLink + movie.poster_path} alt="" width="40%" height="40%"></img>
+                <div style={{ padding: 20 }}><h3>{movie.original_title}</h3></div>
+                <div></div>
+                <p>Rating: {movie.vote_average}</p>
+                <p>Votes: {movie.vote_count}</p>
+                <p>Original language: {movie.original_language}</p>
+                <p>Overview: {movie.overview}</p>
+                <p>Release date: {movie.release_date}</p>
+                <p>Runtime: {movie.runtime}</p>
+                <p>Budget: {movie.budget}</p>
+                <p>Revenue: {movie.revenue}</p>
+                <p>Status: {movie.status}</p>
+                <p>Popularity: {movie.popularity}</p>
+                <p>IMDB ID: {movie.imdb_id}</p>
             </div>
         );
 
--- src/util/requests.js
+++ src/util/requests.js
@@ -4,12 +4,8 @@ const axios = require('axios').default;
 export async function getPopularMovies(pageNum) {
     
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=${pageNum}`;
-    
     const response =  await axios.get(url);
     return response.data;
-    //const response = await fetch(url);
-    const responseJson = await response.json();
-    return responseJson;
 }
 
 export async function getTopRatedMovies(pageNum) {
@@ -21,5 +17,17 @@ export async function getTopRatedMovies(pageNum) {
 export async function getLatestMovies(pageNum) {
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=release_date.desc&page=${pageNum}`;
     const response =  await axios.get(url);
+    return response.data;
+}
+
+export async function getMovie(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+    const response = await axios.get(url);
+    return response.data;
+}
+
+export async function getState(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}/account_states?api_key=${API_KEY}`;
+    const response = await axios.get(url);
     return response.data;
 }
